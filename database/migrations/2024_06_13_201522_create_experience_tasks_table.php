d56f7609981e1ee4bea62613db787a25357aac90
ashwabu <abdulazizredi90@gmail.com>
2024-06-13 23:17:56 +0300

    experience tasks table

--- database/migrations/2024_06_13_201522_create_experience_tasks_table.php
+++ database/migrations/2024_06_13_201522_create_experience_tasks_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('experience_tasks', function (Blueprint $table) {
             $table->id();
+            $table->string("title");
+            $table->string("tasks");
             $table->timestamps();
         });
     }

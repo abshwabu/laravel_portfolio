57a8626320c0f840edb89f12ebd890179fcf446d
ashwabu <abdulazizredi90@gmail.com>
2024-06-13 22:32:00 +0300

    tech stack table

--- database/migrations/2024_06_13_130553_create_tech_stacks_table.php
+++ database/migrations/2024_06_13_130553_create_tech_stacks_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('tech_stacks', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->string("url")->nullable();
+            $table->string("image");
             $table->timestamps();
         });
     }

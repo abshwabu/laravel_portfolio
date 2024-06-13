10949896dab2dd0ee0956f9bd4a90cdeed71cbfb
ashwabu <abdulazizredi90@gmail.com>
2024-06-13 22:59:27 +0300

    images table

--- database/migrations/2024_06_13_130824_create_images_table.php
+++ database/migrations/2024_06_13_130824_create_images_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('images', function (Blueprint $table) {
             $table->id();
+            $table->string("image")->nullable();
+            $table->string("url")->nullable();
             $table->timestamps();
         });
     }
